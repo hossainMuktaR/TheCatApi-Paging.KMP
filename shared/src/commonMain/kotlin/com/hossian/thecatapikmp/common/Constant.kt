2673f56834efb944d98ac5146c416e72caca69cc
Hossain MuktaR <HossainMuktarDev@gmail.com>
2024-09-23 17:54:15 +0600

    Update Constant for api key.kt

--- shared/src/commonMain/kotlin/com/hossian/thecatapikmp/common/Constant.kt
+++ shared/src/commonMain/kotlin/com/hossian/thecatapikmp/common/Constant.kt
@@ -1,5 +1,5 @@
 package com.hossian.thecatapikmp.common
 
 object Constant {
-    const val API_KEY = "live_dYoOVK3ju1CCdZhrj129Uov22imb8yM1CFqyAsRIov1xNiNQPyOIET4oh8qSfT2e"
+    const val API_KEY = "Your-Api-key-xxx"
 }
